003849d54885101532f278f8477304b34dbe765e
manzil.roy@gmail.com <manzil.roy@iais.fraunhofer.de>
2021-06-04 03:33:03 +0200

    changes for sampled points

--- fairnr/modules/encoder.py
+++ fairnr/modules/encoder.py
@@ -616,9 +616,9 @@ class SparseVoxelEncoder(Encoder):
             point_xyz = F.embedding(sampled_idx, point_xyz)
             point_feats = F.embedding(F.embedding(sampled_idx, point_feats), values).view(point_xyz.size(0), -1)
             
-            tpt = torch.unique(sampled_idx)
-            tpt_point_xyz = torch.unique(point_xyz)
-            tpt_sampled_xyz = torch.unique(sampled_xyz)
+            tpt = torch.unique(sampled_idx,dim=0)
+            tpt_point_xyz = torch.unique(point_xyz,dim=0)
+            tpt_sampled_xyz = torch.unique(sampled_xyz,dim=0)
             print(tpt.shape,tpt_point_xyz.shape,sampled_idx.shape,tpt_sampled_xyz.shape, point_xyz.shape)
             # tri-linear interpolation
             p = ((sampled_xyz - point_xyz) / self.voxel_size + .5).unsqueeze(1)
