6828dc38d2be8abe784d3181fb77906aba95060f
manzil.roy@gmail.com <manzil.roy@iais.fraunhofer.de>
2021-06-11 09:45:55 +0200

    bug fix

--- fairnr/modules/encoder.py
+++ fairnr/modules/encoder.py
@@ -646,8 +646,9 @@ class SparseVoxelEncoder(Encoder):
     def cat_pointscolor(self, voxelcolors):
         encoder_states = self.precompute()
         points = encoder_states['voxel_center_xyz']
-        voxels = self.pointcol[:,:3]
-        val = voxelcolors[(voxels[:, None] == points).all(-1).any(-1),:]
+        if self.pointcol is None:
+            voxels = self.pointcol[:,:3]
+            val = voxelcolors[(voxels[:, None] == points).all(-1).any(-1),:]
         if self.pointcol is None:
             self.pointcol = val
         else:
