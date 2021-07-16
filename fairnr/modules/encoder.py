db85dc45e80053e15a573853f5a45f9288aaa81c
manzil.roy@gmail.com <manzil.roy@iais.fraunhofer.de>
2021-07-17 00:25:05 +0200

    bike dataset experiment5, filter white

--- fairnr/modules/encoder.py
+++ fairnr/modules/encoder.py
@@ -663,7 +663,7 @@ class SparseVoxelEncoder(Encoder):
     @torch.no_grad()
     def white_color_separate(self, voxelcolor):
         torch.set_default_tensor_type('torch.cuda.FloatTensor')
-        upper = torch.tensor([[120.0,120.0,120.0]])
+        upper = torch.tensor([[110.0,110.0,110.0]])
         lower = torch.tensor([[0.0,0.0,0.0]])
         voxels = voxelcolor[:,:3]
         colors = voxelcolor[:,3:]*255.0
