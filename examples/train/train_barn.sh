ace82b649dbd3a8e150092d1407a5e4c601cc4a4
manzil.roy@gmail.com <manzil.roy@iais.fraunhofer.de>
2021-09-03 19:13:39 +0200

    train  barn

--- examples/train/train_barn.sh
+++ examples/train/train_barn.sh
@@ -18,7 +18,7 @@ mkdir -p $SAVE/$MODEL
 python train.py ${DATASET} \
     --user-dir fairnr \
     --task single_object_rendering \
-    --train-views "0..50" \
+    --train-views "0..100" \
     --view-resolution $RES \
     --max-sentences 1 \
     --view-per-batch 2 \
@@ -26,7 +26,7 @@ python train.py ${DATASET} \
     --no-preload \
     --sampling-on-mask 1.0 --no-sampling-at-reader \
     --valid-view-resolution $RES \
-    --valid-views "50..100" \
+    --valid-views "100..200" \
     --valid-view-per-batch 1 \
     --transparent-background "1.0,1.0,1.0" \
     --background-stop-gradient \
