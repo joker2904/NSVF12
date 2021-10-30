3f837adca90e529be2cdb3426b9348bcf4f728e1
manzil.roy@gmail.com <manzil.roy@iais.fraunhofer.de>
2021-10-30 14:40:44 +0200

    prune semantic voxel

--- fairnr/modules/encoder.py
+++ fairnr/modules/encoder.py
@@ -837,7 +837,7 @@ class SparseVoxelEncoder(Encoder):
         self.pointcolors = torch.zeros(self.points.shape[0],1)
         #if self.pointcol is not None:
         self.pointcolors = self.pointcol            
-        print('pointspecial: ',self.pointcol.shape,self.pointcolors.shape, self.points.shape)
+        #print('pointspecial: ',self.pointcol.shape,self.pointcolors.shape, self.points.shape)
         self.pointcol = None
         self.keep = self.keep.new_ones(new_point_length)
         logger.info("splitting done. # of voxels before: {}, after: {} voxels".format(points.size(0), self.keep.sum()))
