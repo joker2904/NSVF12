62ee06d0b1b15c2c170d7c5964b2f3d1c6096d70
manzil <manzil.roy@vathos.net>
2021-11-07 15:01:10 +0100

    initial commit of label points

--- fairnr/data/geometry.py
+++ fairnr/data/geometry.py
@@ -254,8 +254,8 @@ def splitting_points(point_xyz, point_feats, values, label0, label1, half_voxel)
     old_coords = discretize_points(point_xyz, quarter_voxel)[0]
     new_coords = offset_points(old_coords).reshape(-1, 3)
     new_keys0  = offset_points(new_coords).reshape(-1, 3)
-    new_label0 = new_points #offset_points(label0).reshape(-1, 3)
-    new_label1 = new_points #offset_points(label1).reshape(-1, 3)
+    new_label0 = offset_points(label0).reshape(-1, 1)
+    new_label1 = offset_points(label1).reshape(-1, 1)
     
     # get unique keys and inverse indices (for original key0, where it maps to in keys)
     new_keys, new_feats = torch.unique(new_keys0, dim=0, sorted=True, return_inverse=True)
